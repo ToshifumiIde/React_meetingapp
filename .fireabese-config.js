e60ea6b6f0bc5359a71a6b14cd8e07f8f215f294
ToshifumiIde <toshifumiide198765@gmail.com>
2021-01-04 12:00:28 +0900

    create signin page

--- .fireabese-config.js
+++ .fireabese-config.js
@@ -1,11 +1,10 @@
 module.exports = {
-    apiKey: "AIzaSyAdyt3l_xYSwnsiFtHFPkx66Bp2t-AlO8k",
-    authDomain: "toshifumiide-idobatakaigi.firebaseapp.com",
-    databaseURL:
-      "https://toshifumiide-idobatakaigi-default-rtdb.firebaseio.com",
-    projectId: "toshifumiide-idobatakaigi",
-    storageBucket: "toshifumiide-idobatakaigi.appspot.com",
-    messagingSenderId: "211846729017",
-    appId: "1:211846729017:web:687b20bfb6c625b2926ce8",
-    measurementId: "G-QJW23W95GW",
-  };
+  apiKey: "AIzaSyAdyt3l_xYSwnsiFtHFPkx66Bp2t-AlO8k",
+  authDomain: "toshifumiide-idobatakaigi.firebaseapp.com",
+  databaseURL: "https://toshifumiide-idobatakaigi-default-rtdb.firebaseio.com",
+  projectId: "toshifumiide-idobatakaigi",
+  storageBucket: "toshifumiide-idobatakaigi.appspot.com",
+  messagingSenderId: "211846729017",
+  appId: "1:211846729017:web:687b20bfb6c625b2926ce8",
+  measurementId: "G-QJW23W95GW",
+};
