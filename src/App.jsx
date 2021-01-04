4362d6b4f2717a5050e70962adbdb8b0cce28ef7
ToshifumiIde <toshifumiide198765@gmail.com>
2021-01-04 20:25:34 +0900

    create config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, {useState} from 'react'
 import {SignIn} from "./components/SignIn";
 import {Main} from "./components/Main";
+import config from "./config.json";
 
 
 export const App = () => {
   const [name , setName] =useState("");
   console.log(`YourName is ${name}`);
 
-  if(name === ""){
+  if(config.signInEnabled && name === ""){
     return <SignIn setName={setName}/> ;
   } else{
     return <Main name={name}/>;
