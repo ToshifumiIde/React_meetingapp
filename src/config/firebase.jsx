d2ef672423d6a1f940fc1904fd5331feee6140db
ToshifumiIde <toshifumiide198765@gmail.com>
2021-01-07 20:26:58 +0900

    remove hard coded env vars

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,14 +1,25 @@
 import firebase from "firebase";
 
+const {
+REACT_APP_FIREBASE_API_KEY,
+REACT_APP_FIREBASE_AUTH_DOMAIN,
+REACT_APP_FIREBASE_DATABASE_URL,
+REACT_APP_FIREBASE_PROJECT_ID,
+REACT_APP_FIREBASE_STORAGE_BUCKET,
+REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+REACT_APP_FIREBASE_APP_ID,
+REACT_APP_FIREBASE_MEASUREMENT_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAdyt3l_xYSwnsiFtHFPkx66Bp2t-AlO8k",
-  authDomain: "toshifumiide-idobatakaigi.firebaseapp.com",
-  databaseURL: "https://toshifumiide-idobatakaigi-default-rtdb.firebaseio.com",
-  projectId: "toshifumiide-idobatakaigi",
-  storageBucket: "toshifumiide-idobatakaigi.appspot.com",
-  messagingSenderId: "211846729017",
-  appId: "1:211846729017:web:c8e88e68eea76ef0926ce8",
-  measurementId: "G-1HKPFK7411",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 firebase.initializeApp(firebaseConfig);//firebaseの初期化処理（configのオブジェクトを渡している）
 const database = firebase.database();  //初期化したのち、リアルタイムデータベースを「全体・丸ごと」参照して、定数databaseに格納。
